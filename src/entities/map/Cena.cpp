f29ba1616286256c18904b61f8650d7aa80c52f7
Laura Silveira Brandao Pinto <laurasbp@ufmg.br>
2026-05-21 12:43:06 -0300

    feat: Adiciona Cena.cpp inicial e comenta TrechoMapa.cpp

---
+++ src/entities/map/Cena.cpp
@@ -0,0 +1,87 @@
+/**
+ * @file Cena.cpp
+ * @brief Implementação da classe Cena
+ */
+#include "Cena.hpp"
+
+/**
+ * @brief Construtor da classe Cena.
+ */
+Cena::Cena(int nivel, TrechoMapa* trecho):_nivel(nivel),_trechoAtual(trecho), _inventarioAberto(false),_emBatalha(false),_explorando(true){
+
+}
+
+/**
+ * @brief Inicializa a cena
+ */
+void Cena::iniciarCena(){
+}
+
+/**
+ * @brief Finaliza a cena.
+ */
+void Cena::finalizarCena(){
+}
+
+/**
+ * @brief Verifica a continuidade da cena
+ */
+bool Cena::verificarContinuidade() const{
+    return false;
+}
+
+/**
+ * @brief Gerencia a movimentação no mapa.
+ */
+void Cena::andar(){
+
+}
+
+/**
+ * @brief Permite vasculhar o trecho atual do mapa.
+ */
+void Cena::vasculhar(){
+
+}
+
+/**
+ * @brief Realiza as interações com os NPCs de diálogo
+ */
+void Cena::interagirNPCs(){
+
+}
+
+/**
+ * @brief Abre o inventário do jogador
+ */
+void Cena::abrirInventario(){
+
+}
+
+/**
+ * @brief Inicializa uma batalha
+ */
+void Cena::iniciarBatalha(){
+
+}
+
+/**
+ * @brief Retorna se o inventário está aberto.
+ */
+bool Cena::inventarioAberto() const{
+    return _inventarioAberto;
+}
+
+/**
+ * @brief Retorna se o player está em batalha.
+ */
+bool Cena::emBatalha() const{
+    return _emBatalha;
+}
+
+/**
+ * @brief Retorna se o jogador está explorando (vasculhando, andando, interagindo com NPCs)
+ */
+bool Cena::explorando() const{
+    return _explorando;
+}
--- src/entities/map/TrechoMapa.cpp
+++ src/entities/map/TrechoMapa.cpp
@@ -1,29 +1,55 @@
+/**
+ * @file TrechoMapa.cpp
+ * @brief Implementação da classe TrechoMapa
+ */
+
 #include "TrechoMapa.hpp"
 
+/**
+ * @brief Construtor da classe TrechoMapa.
+ * Inicializa os atributos básicos do trecho
+ */
 TrechoMapa::TrechoMapa(int id, std::string descricao):_id(id), _cena(nullptr),_descricao(descricao),_aberto(false){
 
 }
-
+/**
+ * @brief Marca o trecho como aberto.
+ */
 void TrechoMapa::abrirTrecho(){
-    _aberto = true;
+    
 }
 
+/**
+ * @brief Adiciona um item ao trecho.
+ */
 void TrechoMapa::adicionarItem(Item* item){
-    _itens.push_back(item);
+    
 }
 
+/**
+ * @brief Adiciona um NPC ao trecho
+ */
 void TrechoMapa::adicionarNPC(Personagem* npc){
-    _npcs.push_back(npc);
+    
 }
 
+/**
+ * @brief Retorna os itens presentes no trecho
+ */
 std::vector<Item*> TrechoMapa::pegarItens() const{
-    return _itens;
+    return {};
 }
 
+/**
+ * @brief Retorna os NPCs presentes no trecho.
+ */
 std::vector<Personagem*> TrechoMapa::pegarNPCs() const{
-    return _npcs;
+    return {};
 }
 
+/**
+ * @brief Verifica se o trecho já foi aberto.
+ */
 bool TrechoMapa::estaAberto() const{
-    return _aberto;
+    return {};
 }
