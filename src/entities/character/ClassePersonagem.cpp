e3beb5e9f5932c726b89e8b953326494e26a7587
Matheus Mariano Euzébio <matheusmarianoe@gmail.com>
2026-05-21 18:41:32 -0300

    feat: adiciona Personagem.cpp e ClassePersonagem.cpp

---
+++ src/entities/character/ClassePersonagem.cpp
@@ -0,0 +1,53 @@
+#include "ClassePersonagem.hpp"
+
+ClassePersonagem::ClassePersonagem(TipoClasse tipo) : _tipo(tipo) {
+    _descricao = "";
+    _arma = "";
+};
+
+std::string ClassePersonagem::getNome() const {
+    switch(_tipo) {
+        case TipoClasse::Arqueiro:
+            return "Arqueiro";
+
+        case TipoClasse::Guerreiro:
+            return "Guerreiro";
+
+        case TipoClasse::Mago:
+            return "Mago";
+
+        case TipoClasse::Tanque:
+            return "Tanque";
+    }
+    return "";
+}
+
+std::string ClassePersonagem::getDescricao() const {
+    return _descricao;
+}
+
+std::string ClassePersonagem::getArma() const {
+    return _arma;
+}
+
+
+TipoClasse ClassePersonagem::getTipo() const {
+    return _tipo;
+}
+
+const std::array<Ataque, 3>& ClassePersonagem::getAtaques() const {
+    return _ataques;
+}
+
+const Ataque& ClassePersonagem::getAtaque(TipoAtaque tipo) const {
+    switch(tipo) {
+        case TipoAtaque::Simples:
+            return _ataques[0];
+        case TipoAtaque::Rapido:
+            return _ataques[1];
+        case TipoAtaque::Forte:
+            return _ataques[2];
+    }
+    return _ataques[0];
+}
+
---
+++ src/entities/character/Personagem.cpp
@@ -0,0 +1,112 @@
+#include "Personagem.hpp"
+
+Personagem::Personagem(
+    std::string nome,
+    std::string descricao,
+    std::string fala,
+    double ataque, 
+    double defesa, 
+    double vidaTotal, 
+    double ppTotal, 
+    double agilidade, 
+    TipoClasse tipoClasse,
+    TipoPersonagem tipo,
+    int nivel)
+    : _nome(nome),
+      _descricao(descricao),
+      _fala(fala),
+      _ataque(ataque),
+      _defesa(defesa),
+      _vidaTotal(vidaTotal),
+      _vidaAtual(vidaTotal),
+      _ppTotal(ppTotal),
+      _ppAtual(ppTotal),
+      _agilidade(agilidade),
+      _xp(0),
+      _classe(tipoClasse),
+      _tipo(tipo),
+      _nivel(nivel)
+{}
+
+void Personagem::receberDano(double dano) {
+
+}
+
+void Personagem::recuperarVida(double cura) {
+
+}
+
+void Personagem::recuperarMana(double quantidadeMana) {
+
+}
+
+void Personagem::gastarMana(double custoMana) {
+
+}
+
+void Personagem::ganharXp(double quantidadeXp) {
+
+}
+
+void Personagem::subirNivel() {
+
+}
+
+bool Personagem::estaVivo() const {
+    if(_vidaAtual == 0)
+        return false;
+    else
+        return true;
+}
+
+void Personagem::cenaAtual() {
+
+}
+
+std::string Personagem::getNome() const {
+    return _nome;
+}
+
+std::string Personagem::getDescricao() const {
+    return _descricao;
+}
+
+std::string Personagem::getFala() const {
+    return _fala;
+}
+
+double Personagem::getVidaAtual() const {
+    return _vidaAtual;
+}
+
+double Personagem::getAtaque() const {
+    return _ataque;
+}
+
+double Personagem::getDefesa() const {
+    return _defesa;
+}
+
+double Personagem::getAgilidade() const {
+    return _agilidade;
+}
+
+double Personagem::getManaAtual() const {
+    return _ppAtual;
+}
+
+int Personagem::getNivel() const {
+    return _nivel;
+}
+
+double Personagem::getXp() const {
+    return _xp;
+}
+
+TipoPersonagem Personagem::getTipo() const {
+    return _tipo;
+}
+
+const ClassePersonagem& Personagem::getClasse() const {
+    return _classe;
+}
