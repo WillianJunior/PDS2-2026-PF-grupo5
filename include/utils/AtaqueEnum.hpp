439b1c18370b2307651a0ff949cfab62539546eb
Matheus Mariano Euzébio <matheusmarianoe@gmail.com>
2026-06-20 21:14:57 -0300

    refactor: atualiza documentação de AtaqueEnum.hpp

--- include/utils/AtaqueEnum.hpp
+++ include/utils/AtaqueEnum.hpp
@@ -11,40 +11,40 @@
 
 /**
  * @enum IdAtaque
- * @brief Identifica um ataque, 
+ * @brief Identifica um ataque por um id.
  */
 enum class IdAtaque {
     // Arqueiro
-    Flecha,
-    TiroRapido,
-    FlechaExplosiva,
-    TiroCerteiro,
-    Saraivada,
-    TiroFinal,
+    Flecha,             //< Ataque Simples
+    TiroRapido,         //< Ataque Rapido
+    FlechaExplosiva,    //< Ataque Forte 1
+    TiroCerteiro,       //< Ataque Forte 2
+    Saraivada,          //< Ataque Forte 3
+    TiroFinal,          //< Ataque Forte 4
 
     // Guerreiro
-    Espada,
-    GolpeRelampago,
-    FuriaBerserker,
-    GolpeDevastador,
-    Perfuracao,
-    GolpeDoAbismo,
+    Espada,             //< Ataque Simples
+    GolpeRelampago,     //< Ataque Rapido
+    FuriaBerserker,     //< Ataque Forte 1
+    GolpeDevastador,    //< Ataque Forte 2
+    Perfuracao,         //< Ataque Forte 3
+    GolpeDoAbismo,      //< Ataque Forte 4
 
     // Mago
-    Truque,
-    ProjetilArcano,
-    BolaDeFogo,
-    Raio,
-    DrenoDaMagia,
-    VorticeArcano,
+    Truque,             //< Ataque Simples
+    ProjetilArcano,     //< Ataque Rapido
+    BolaDeFogo,         //< Ataque Forte 1
+    Raio,               //< Ataque Forte 2
+    DrenoDaMagia,       //< Ataque Forte 3
+    VorticeArcano,      //< Ataque Forte 4
 
     // Tanque
-    Escudo,
-    Investida,
-    PancadaDeEscudo,
-    Terremoto,
-    BarreiraDeEspinhos,
-    Martirio
+    Escudo,             //< Ataque Simples
+    Investida,          //< Ataque Rapido
+    PancadaDeEscudo,    //< Ataque Forte 1
+    Terremoto,          //< Ataque Forte 2
+    BarreiraDeEspinhos, //< Ataque Forte 3
+    Martirio            //< Ataque Forte 4
 };
 
 #endif
